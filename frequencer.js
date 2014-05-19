b96fcd4b22a57337b127a93b2f645562f7da4063
ilyabasiuk <ilyabasiuk@gmail.com>
2014-05-19 19:37:02 +0300

    added events

--- frequencer.js
+++ frequencer.js
@@ -8,6 +8,7 @@ var frequencer = function(config) {
        var timerId,
            lastActionTime = Date.now(),
            cache = {},
+           eventHandlers = {},
            isPromise = function (value) {
                 return !!value && typeof value === 'object' && value.hasOwnProperty('done') && value.hasOwnProperty('fail');
            },
@@ -16,15 +17,15 @@ var frequencer = function(config) {
            getFrequency = function () {
                return Math.min.apply(null, frequencies);
            },
-           step = function() {
+           step = function(immediate) {
                var result;
                lastActionTime = Date.now(),
-               frequency = getFrequency(),
+               frequency = immediate?0 :getFrequency(),
                activeFreq = [];
 
                frequencies.forEach(function(freq) {
                     if (cache[freq]) {
-                      if ((Date.now() -cache[freq])> freq) {
+                      if ((Date.now() - cache[freq])> freq) {
                           cache[freq] = Date.now();
                           activeFreq.push(freq);
                       }
@@ -34,7 +35,9 @@ var frequencer = function(config) {
                       activeFreq.push(freq);
                     }
                });
+               fireEvent("waiting");
                timerId = setTimeout(function() {
+                  fireEvent("working");
                   result = action(activeFreq); ///  pass frequency array here
                   if (isPromise(result)){
                      result.done(step);
@@ -42,22 +45,58 @@ var frequencer = function(config) {
                      step();
                   }
                }, frequency);
+           },
+           fireEvent = function(eventName) {
+               if (eventHandlers.hasOwnProperty(eventName)){
+                   eventHandlers[eventName].forEach(function(callback) {
+                       callback();
+                   })
+               }
            };
 
 
     return {
        run : function() {
            console.log("ran");
-           step();
+           step(true);
        },
-       setFrequency: function() {
-
+       setFrequency: function(frequency, immediate) {
+           if (frequencies.indexOf(frequency) <0 ) {
+               frequencies.push(frequency);
+               if (immediate) {
+                   clearTimeout(timerId);
+                   step(true);
+               }
+           }
        },
        stop : function () {
-
+           clearTimeout(timerId);
        },
-       attachEvent : function() {
-
+       addEvent : function(eventName, callback) {
+           //waiting
+           //working
+           if (eventHandlers.hasOwnProperty(eventName)){
+               eventHandlers[eventName].push(callback);
+           } else {
+               eventHandlers[eventName] = [callback];
+           }
+       },
+       removeEvent : function(eventName, callback) {
+           var i, currentEvent;
+           if (eventHandlers.hasOwnProperty(eventName)){
+               currentEvent = eventHandlers[eventName];
+               if (callback){
+                   for (i = 0; i < currentEvent.length; i++){
+                       if (currentEvent[i] === callback){
+                           currentEvent.splice(i, 1);
+                           break;
+                       }
+                   }
+               } else {
+                   currentEvent.length = 0;
+                   delete eventHandlers[eventName];
+               }
+           }
        },
        getFrequncies: function() {
            return frequencies;
